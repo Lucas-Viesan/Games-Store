bd869eb7d2016219dac3339d1746b387f6729d69
Lucas-Viesan <vieiratm01@hotmail.com>
2025-01-16 17:49:11 -0300

    Implementado CRUD de produtos

--- src/main/java/com/generation/gamesstore/model/Produto.java
+++ src/main/java/com/generation/gamesstore/model/Produto.java
@@ -27,4 +27,46 @@ public class Produto {
 	
 	@Column(nullable = false)
 	private Integer quantidadeEstoque;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+	
+	
 }
